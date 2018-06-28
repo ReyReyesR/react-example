3254de4bd2322eefd011946c9b2f5a2ddd2d870b
Reynaldo Reyes <Reynaldo.Reyes.4@gmail.com>
2018-06-28 11:33:04 +0100

    Add starteditexpense code and test cases

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -29,6 +29,17 @@ export const startAddExpense = (expenseData = {}) => {
   };
 };
 
+export const startEditExpense = (id, updates) => {
+  return dispatch => {
+    return database
+      .ref(`expenses/${id}`)
+      .update(updates)
+      .then(() => {
+        dispatch(editExpense(id, updates));
+      });
+  };
+};
+
 export const editExpense = (id, updates) => ({
   type: "EDIT_EXPENSE",
   id,
--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -7,7 +7,8 @@ import {
   removeExpense,
   setExpenses,
   startSetExpenses,
-  startRemoveExpense
+  startRemoveExpense,
+  startEditExpense
 } from "../../actions/expenses";
 import expenses from "../fixtures/expenses";
 import database from "../../firebase/firebase";
@@ -140,6 +141,27 @@ test("Should remove expenses from firebase", done => {
     })
     .then(snapshot => {
       expect(snapshot.val()).toBeFalsy();
+      done();
+    });
+});
+
+test("Should edit expense from firebase", done => {
+  const store = createMockStore({});
+  const id = expenses[0].id;
+  const updates = { amount: 123456 };
+  store
+    .dispatch(startEditExpense(id, updates))
+    .then(() => {
+      const actions = store.getActions();
+      expect(actions[0]).toEqual({
+        type: "EDIT_EXPENSE",
+        id,
+        updates
+      });
+      return database.ref(`expenses/${id}`).once("value");
+    })
+    .then(snapshot => {
+      expect(snapshot.val().amount).toBe(updates.amount);
       done();
     });
 });
